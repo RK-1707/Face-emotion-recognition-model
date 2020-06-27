25fbff8f598b3845decd2ed44a19f7c107eb72d3
Rohan Kumar <rohankumar1707@gmail.com>
2020-06-27 21:20:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ from tensorflow.keras.utils import plot_model
 
 from IPython.display import SVG, Image
 from livelossplot import PlotLossesTensorFlowKeras
+from sklearn.metrics import confusion_matrix
+import itertools
 import tensorflow as tf
 print(" Tensorflow version:", tf.__version__ )
 
@@ -101,7 +103,66 @@ history = model.fit(
 	callbacks= callbacks
 )
 
+
 #represent model as JSON string
 model_json=model.to_json()
 with open("model.json", "w") as json_file:
 	json_file.write(model_json)
+
+
+	      
+# plot the evolution of Loss and Acuracy on the train and validation sets
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(20,10))
+plt.subplot(1, 2, 1)
+plt.suptitle('Optimizer : Adam', fontsize=10)
+plt.ylabel('Loss', fontsize=16)
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.legend(loc='upper right')
+
+plt.subplot(1, 2, 2)
+plt.ylabel('Accuracy', fontsize=16)
+plt.plot(history.history['acc'], label='Training Accuracy')
+plt.plot(history.history['val_acc'], label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.show()
+	      
+	      
+# show the confusion matrix of our predictions
+# compute predictions
+predictions = model.predict_generator(generator=validation_generator)
+y_pred = [np.argmax(probas) for probas in predictions]
+y_test = validation_generator.classes
+class_names = validation_generator.class_indices.keys()
+
+def plot_confusion_matrix(cm, classes, title='Confusion matrix', cmap=plt.cm.Blues):
+    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    plt.figure(figsize=(10,10))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+    
+# compute confusion matrix
+cnf_matrix = confusion_matrix(y_test, y_pred)
+np.set_printoptions(precision=2)
+
+# plot normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=class_names, title='Normalized confusion matrix')
+plt.show()
